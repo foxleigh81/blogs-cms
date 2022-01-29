cb8386a31180e0df3b03c35289360e8663118ba8
Alexander Foxleigh <alex@alexfoxleigh.com>
2022-01-29 23:12:14 +0000

    Adds CDN for media

--- config/plugins.js
+++ config/plugins.js
@@ -1,6 +1,20 @@
-module.exports = {
+module.exports = ({env}) => ({
   'import-content': {
     enabled: true,
     resolve: './src/plugins/import-content'
   },
-}
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: 'foxleigh',
+        api_key: '162582351145869',
+        api_secret: 'CduF40wxKriO4mtiyzTi2_YHH2U',
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+});
