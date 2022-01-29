236a35c23cc6bf322523ac2005a4b1ddd6a58753
Alexander Foxleigh <alex@alexfoxleigh.com>
2022-01-29 18:26:40 +0000

    Adds environment variables to config

--- config/database.js
+++ config/database.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'strapi'),
-      password: env('DATABASE_PASSWORD', 'kindaprivate'),
+      host: env('DATABASE_HOST', process.env.DATABASE_HOST),
+      port: env.int('DATABASE_PORT', process.env.DATABASE_PORT),
+      database: env('DATABASE_NAME', process.env.DATABASE_NAME),
+      user: env('DATABASE_USERNAME', process.env.DATABASE_USERNAME),
+      password: env('DATABASE_PASSWORD', process.env.DATABASE_PASSWORD),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
