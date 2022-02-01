fb0bbd267b96e24b0597a07053dde60f39fcb4b0
Alexander Foxleigh <alex@alexfoxleigh.com>
2022-02-01 16:00:43 +0000

    Adds config sync plugin

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '54554add97370c5d2606bf606aff1a05'),
   },
+  watchIgnoreFiles: [
+    '**/config/sync/**',
+  ],
 });
